3af03b67203b938f2d6bd7ab98ae7ccbbbc70623
François-Xavier Suarez <francoisxaviersuarez@gmail.com>
2025-03-12 21:35:13 +0100

    Apply prettier and setup pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ import users from "#src/routes/users";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", users);
 
 app.get("/", (_, res: Response) => {
-    res.json({ message: "Welcome to the Express + TypeScript Server!" });
+  res.json({ message: "Welcome to the Express + TypeScript Server!" });
 });
 
 app.listen(PORT, () => {
-    console.log("The server is running");
+  console.log("The server is running");
 });
